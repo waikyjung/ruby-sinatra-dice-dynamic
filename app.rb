cf625449f8cd1b3159de5ec6abca912d70335e9a
waikyjung <waiky.jung@gmail.com>
2024-10-17 04:40:28 +0000

    final touches

--- app.rb
+++ app.rb
@@ -5,55 +5,7 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..6)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
-end
-
-get("/dynamic/:num_dies/:num_sides") do
+get("/dice/:num_dies/:num_sides") do
   @dies = params.fetch("num_dies").to_i
   @sides = params.fetch("num_sides").to_i
   
